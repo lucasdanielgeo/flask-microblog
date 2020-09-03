019c0653fabc68dc772670d22b5fc11800c3d80f
Lucas Barros Esteves Daniel <223136@IPUFNT.local>
2020-09-03 14:56:26 -0300

    chapter 3 - initial

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user={'username':'Lucas'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Lucas'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beatiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avangers movie was so cool!'
+        }
+    ]
+    
+    return render_template('index.html', title='Home', user=user, posts=posts)
