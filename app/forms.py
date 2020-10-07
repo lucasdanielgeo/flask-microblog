fffe0757891a06239c747f0a5daf1a03fb24d325
Lucas Daniel <lucasdaniel.geo@gmail.com>
2020-10-07 18:55:06 -0300

    Chapter 6 - Profile Editor - unfinished

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, TextAreaField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError, Email, EqualTo
 from app.models import User
+
 
 
 class LoginForm(FlaskForm):
@@ -29,3 +30,7 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please, use a different email address')
 
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
